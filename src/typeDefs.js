0bfd74affa8a41067f7fe720c84deb5c62b3426d
Paola Garay <garay.angela@gmail.com>
2020-12-30 10:13:21 -0500

    Add resolvers for mutations deleteLink and  updateLink

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,7 +1,22 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }
+
     type Query {
         info: String!
+        feed: [Link!]!
+        link(id: ID!): Link
+    }
+
+    type Mutation {
+        post(url: String!, description: String!): Link!
+        updateLink(id: ID!, url: String, description: String): Link
+        deleteLink(id: ID!): Link
     }
 `;
