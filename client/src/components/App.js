9a36a59ccf1b40af1bbc98105e9ab035618f73e7
Paola Garay <garay.angela@gmail.com>
2021-01-03 20:05:31 -0500

    Getting data from the DB by  using apollo client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,39 @@
 
 import '../styles/App.css';
+import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+
+const GET_LINKS = gql `
+    query {
+      feed {
+        _id
+        description
+        url
+      }
+    }
+`
 
 function App() {
+
+  const getLinksData = useQuery(GET_LINKS);
+
+  const allLinksHandler = () => (
+      getLinksData.data ?
+        getLinksData.data.feed.map((item, i)=>(
+        <div key={i}>
+          <h4>{item.url}</h4>
+          <p>{item.description}</p>
+        </div>
+        ))
+      :null
+  )
+
+
   return (
     <div>
-      APP
+      <h3>All Links</h3>
+        {allLinksHandler()}
     </div>
   );
 }
